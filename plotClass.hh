e7da3beff7632a321e1c655d311c2282563559ff
Urs Langenegger <urs.langenegger@psi.ch>
2014-08-22 16:03:40 +0200

    add overlays

--- plotClass.hh
+++ plotClass.hh
@@ -49,8 +49,8 @@ public :
   // -- overlays and normalizing histograms
   void           normHist(TH1 *, std::string ds="", int method = NONORM); 
   virtual void   overlayAll();
-  virtual void   overlay(TH1* h1, std::string f1, TH1 *h2, std::string f2, bool legend = true);
-  virtual void   overlay(std::string f1, std::string h1name, std::string f2, std::string h2name, bool legend = true);
+  void           overlay(TH1* h1, std::string f1, TH1 *h2, std::string f2, bool legend = true);
+  void           overlay(std::string h1name, std::string f1, std::string h2name, std::string f2, bool legend = true);
 
   virtual void   bookHist(std::string name); 
 
